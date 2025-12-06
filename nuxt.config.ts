137bfa7984537734ce6b03e1359434d24a2faeb5
mao.qiu <2939561428@qq.com>
2025-12-06 20:02:23 +0800

    feat: 更新热力图

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -126,11 +126,11 @@ export default defineNuxtConfig({
           ? {}
           : {
               innerHTML: `
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "ubxx0mgosj");`,
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "uh9pi3oif4");`,
               tagPriority: "critical",
               tagPosition: "head",
               async: true,
