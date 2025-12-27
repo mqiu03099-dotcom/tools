b65df0eb00bda1d8f5136fd199b1c36bc488f0c6
mao.qiu <2939561428@qq.com>
2025-12-27 20:10:29 +0800

    feat: 优化图片

--- components/MHero/index.vue
+++ components/MHero/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="hero min-h-[400px] sm:min-h-[450px] md:min-h-[500px] rounded-[var(--radius-box)] overflow-hidden capitalize"
+    class="hero min-h-[400px] sm:min-h-[450px] md:min-h-[500px] bg-center! rounded-[var(--radius-box)] overflow-hidden capitalize"
     :style="`background: url(${tool?.bgImg}) no-repeat;`"
   >
     <div class="hero-overlay"></div>
