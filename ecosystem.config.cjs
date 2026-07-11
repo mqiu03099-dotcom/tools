832482241fc4b16e786105123954513b9fabc2eb
mao.qiu <2939561428@qq.com>
2026-07-11 20:05:25 +0800

    chore: 清理后端运行配置

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,14 +8,7 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         HOST: "0.0.0.0",
-        PORT: "3004",
-        POSTGRES_HOST: "localhost",
-        POSTGRES_PORT: "5432",
-        POSTGRES_DATABASE: "milk_frog_admin",
-        POSTGRES_USER: "postgres",
-        POSTGRES_PASSWORD: "root",
-        WECHAT_APP_ID: "wx394b878feb6c7786",
-        WECHAT_APP_SECRET: "84f13a5aed45bf9bd7a0c5969a9fa6e1"
+        PORT: "3004"
       },
       error_file: "./local/pm2-error.log",
       out_file: "./local/pm2-out.log",
